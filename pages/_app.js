9114dd3e533dbd052579be08fb61d92e0cf6f5b0
thecryptodiver <thecryptodiver@protonmail.com>
2022-01-22 21:49:07 +0530

    add seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
+import { Fragment } from 'react';
+import { DefaultSeo } from 'next-seo';
 import '../assets/styles/index.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <DefaultSeo
+        title="Ground Zero"
+        description="Learn basics of the crypto ecosystem for free!"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://groundzero.superteam.fun/',
+          site_name: 'Ground Zero',
+          images: [
+            {
+              url: '/images/bg-image.jpeg',
+              width: 800,
+              height: 600,
+              alt: 'Ground Zero',
+              type: 'image/jpeg',
+            }
+          ]
+        }}
+        twitter={{
+          handle: '@SuperteamDAO',
+          site: '@SuperteamDAO',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
